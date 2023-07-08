f30607fe1459b13009f0117d32c2c2c80b63ccb6
Dennis Kipchumba kipyegon <106525815+denztivoh@users.noreply.github.com>
2023-07-08 14:31:26 +0300

    Add files via upload

---
+++ app/core/config.php
@@ -0,0 +1,4 @@
+<?php
+define('ROOT', 'http://localhost/blog/public');
+
+define('APP_NAME', "my_blog");
---
+++ app/core/innit.php
@@ -0,0 +1,5 @@
+<?php
+//load required files
+require "config.php";
+require "connection.php";
+require "functions.php";
